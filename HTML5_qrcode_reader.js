e92ccf3343e5d8fb67e4e309054f359fcfec53d9
RodBrowning <rodrigo.browning@gmail.com>
2022-04-02 16:44:14 -0300

    funcão que retorna objeto de configuração da função html5QrCode.start com valores de aspectratio e qrbox calculados sob medida para o componente que o comportara

--- HTML5_qrcode_reader.js
+++ HTML5_qrcode_reader.js
@@ -1,29 +1,32 @@
-
-
 function init(html5QrCode) {
     const readCodeBtn = document.querySelector(".reader-btn");
     const closePopUpBtn = document.querySelector(".close-popup-btn");
-    const closePopUpResultBtn = document.querySelector(".close-popup-result-btn");
-    const popUpResultWrapper = document.querySelector(".pop-up-result-wrapper")
+    const closePopUpResultBtn = document.querySelector(".close-result-popup-btn");
+    const popUpResultWrapper = document.querySelector(".result-pop-up-wrapper")
     const popUpWrapper = document.querySelector(".pop-up-wrapper")
-    const popUpBox = popUpWrapper.querySelector(".pop-up-box")
-    const cameraContent = popUpBox.querySelector(".cameraContent")
+    const cameraPopUpBox = popUpWrapper.querySelector(".camera-pop-up-box")
+    const cameraContent = cameraPopUpBox.querySelector(".cameraContent")
     // const qrInput = formPopup.querySelector("#qrCode")
     
     readCodeBtn.addEventListener("click", function () {
-        togglePopup()
-        startQrCodeReader()
+        showPopup()
+        let config = Html5QrCodeConfig(cameraPopUpBox);
+        startQrCodeReader(config)
     })
     closePopUpBtn.addEventListener("click", function () {
         try {
             html5QrCode.stop()
-            togglePopup()
+            hidePopup()
         } catch (error) {
             console.error("Espere a camera iniciar para fechar a janela -->> ", error);
         }
     })
-    function togglePopup() {
-        popUpWrapper.classList.toggle("hide")
+    
+    function hidePopup() {
+        popUpWrapper.classList.add("hide")
+    }  
+    function showPopup() {
+        popUpWrapper.classList.remove("hide")
     }  
 
     closePopUpResultBtn.addEventListener("click", function () {
@@ -32,43 +35,63 @@ function init(html5QrCode) {
     function hideResult() {
         popUpResultWrapper.classList.add("hide")        
     }
-    function showResult() {
-        popUpResultWrapper.classList.remove("hide")        
+    function isCodigoNovo(decodedResult) {
+        // Consultar se resultado existe no banco
+        // Se não, salvar e retornar true;
+        // Se se sim retorna falso;
+    }
+    function showResult(tipoDeFeedback) {
+        // Show feedback positivo se true
+        // Show feedback negativo se false
+        popUpResultWrapper.classList.remove("hide")
     }
 
-    function startQrCodeReader() {
+    function Html5QrCodeConfig(cameraPopUpBox) {
+        var popUpWidth = cameraPopUpBox.offsetWidth
+        var popUpHeight = cameraPopUpBox.offsetHeight
+        
+        if(popUpWidth > 590){
+            var widthPercentage = 0.5
+            var heightPercentage = 0.7
+        } else {
+            var widthPercentage = 0.7
+            var heightPercentage = 0.8
+        }
+
+        return {
+            fps: 10,    // Optional, frame per seconds for qr code scanning
+            aspectRatio: (popUpWidth / popUpHeight),
+            qrbox: { 
+                width: (popUpWidth * widthPercentage), 
+                height: (popUpHeight * heightPercentage)
+            }  // Optional, if you want bounded box UI
+        }
+         
+    }
+    function startQrCodeReader(config) {
         Html5Qrcode.getCameras().then(devices => {
             if (devices && devices.length) {
                 var cameraId = devices[0].id;
                 return cameraId
             }
         }).then(cameraId => {
-            // let aspectRatio = screen.width > 590 ? 1.645714 : 1;
-            let aspectRatio = screen.width > 590 ? 2 : 0.65;
-            let width = screen.width > 590 ? 600 : 200;
-            let height = screen.width > 590 ? 350 : 300;
-// debugger
-            html5QrCode.start(cameraId, 
-            {
-                fps: 10,    // Optional, frame per seconds for qr code scanning
-                aspectRatio,
-                qrbox: { width, height }  // Optional, if you want bounded box UI
-            },
+            html5QrCode.start(cameraId, config,
             (decodedText, decodedResult) => {
                 closePopUpBtn.click();
-    
-                console.log("decodedText: ",decodedText);
-                showResult();
+                let tipoDeFeedback = isCodigoNovo(decodedResult);
+                showResult(tipoDeFeedback);
                 html5QrCode.stop()
             },
             (errorMessage) => {
                 // parse error, ignore it.
             })
             .catch((err) => {
-            // Start failed, handle it.
+                hidePopup()
             });
         
-        }).catch(err => { });
+        }).catch(err => { 
+            hidePopup()
+        });
     }
 
 }
